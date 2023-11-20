b8353ae9cf896c1a3ccba807740413d643c57490
louie <louiecasula@gmail.com>
2023-11-19 22:13:58 -0500

    Part 1 done.

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,4 +1,19 @@
 package io.zipcoder.polymorphism;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MainApplication {
+    static Scanner scan = new Scanner(System.in);
+    static ArrayList<String> mascotas = new ArrayList<>();
+    public static void main (String... args){
+        System.out.print("Hola! Cuantas mascotas tienes? ");
+        int cuantidad = scan.nextInt();
+        scan.nextLine();
+        for (int i = 1; i <= cuantidad; i++){
+            System.out.printf("Como se llama mascota #%d? ", i);
+            mascotas.add(scan.nextLine());
+        }
+        System.out.print(mascotas);
+    }
 }
